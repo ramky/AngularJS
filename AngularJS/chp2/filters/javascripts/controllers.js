a545e8abdce53f9f3bd6da4c7e59e93f728bc17d
Ram Iyer <ramky.iyer@yahoo.com>
2015-01-05 13:57:51 -0800

    adding example for custom filters

---
+++ AngularJS/chp2/filters/javascripts/controllers.js
@@ -0,0 +1,18 @@
+'use strict';
+(function (){
+  var homeModule = angular.module('HomeModule', []);
+  homeModule.filter('titleCase', function() {
+    var titleCaseFilter = function(input) {
+      var words = input.split(' ');
+      for (var i = 0; i < words.length; i++){
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      }
+      return words.join(' ');
+    }
+    return titleCaseFilter;
+  });
+
+  homeModule.controller('HomeController', ['$scope', function($scope) {
+    $scope.pageHeading = 'behold the majesty of your page title.'
+  }]);
+})();
